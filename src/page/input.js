59eaba20b1b96b9fb0b57232eba0de6c8550e054
tanhua2019 <534066776@qq.com>
2019-11-14 15:57:47 +0800

    Props, State 与 render 函数

--- src/page/input.js
+++ src/page/input.js
@@ -1,7 +1,3 @@
-# Props, State 与 render 函数
-- react是由数据驱动的一门框架 当数据发生变化，页面跟着发生变化
-- 当组件的state或者props发生变化的时候，render函数就会重新执行
-```JavaScript
 import React, { Component } from 'react'
 
 class Test extends Component {
@@ -37,6 +33,3 @@ export default class Input extends Component {
     })
   }
 }
-```
-1. onChange监听input值的改变，改变及处罚，e.target找到改变的input Dom 获取他的值改变state的值
-2. 首先页面首次加载的时候会调用一次render,之后每次改变input中输入的值render就调用一次
